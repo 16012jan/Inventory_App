2b92fe87a9becd67d374b4ace34fcaa72212f575
igorto <igortonachov@gmail.com>
2017-06-04 19:20:36 +0300

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'angular-app',
+    templateUrl: 'app.template.html'
+})
+
+export class AppComponent {
+}
